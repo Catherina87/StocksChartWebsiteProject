a818d6b3e16e2e6458919d0a216cf445a500214d
Catherina <catherine.pchelina@gmail.com>
2020-06-28 18:02:54 -0700

    Add hardcoded stocks list

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,28 @@ import Stock from './model/Stock';
 
 const App = () => {
 
-  const [stocksList, setStocksList] = useState<Stock[]>([]);
+  const stocks = [
+    {
+      id: 1234567,
+      tiker: "AAPL",
+      buyPrice: "250",
+      numShares: "10"
+    },
+    {
+      id: 12345678,
+      tiker: "GOOG",
+      buyPrice: "400",
+      numShares: "20"
+    },
+    {
+      id: 123456789,
+      tiker: "MSFT",
+      buyPrice: "100",
+      numShares: "5"
+    }
+  ]
+
+  const [stocksList, setStocksList] = useState<Stock[]>(stocks);
 
   const addStock = (stock: Stock) => {
     const newStock: Stock = {
