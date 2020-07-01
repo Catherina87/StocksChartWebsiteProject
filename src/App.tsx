be3e21cfd10a9a55e71ff54c27ed87737ea67e9e
Catherina <catherine.pchelina@gmail.com>
2020-06-30 19:33:05 -0700

    Update styles for home page and add details to My Stocks page

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,8 @@ import { TestDetail } from './components/TestDetail'; // Remove later
 import Stock from './model/Stock';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { StockDetail } from './components/StockDetail';
+import ListGroup from 'react-bootstrap/ListGroup';
+import Image from './image1.jpg';
 
 const App = () => {
 
@@ -73,10 +75,48 @@ const App = () => {
 }
 
 
-const Home = () => (
-  <div>
+const Home = () => {
+
+  const categories = ['Tech', 'Finance', 'Bonds', 'Real Etate', 'Energy']
+
+  return (
+    <div>
     <h1>Home Page</h1>
+    <div className="d-flex flex-row">
+      <div className="col-md-6 text-center">
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis, in!</p>
+        <img src={Image} className="img-fluid" alt="Responsive image"></img>
+      </div>
+
+      <div className="col-md-6 text-center">
+        <h3>Industry Sectors</h3>
+        <ListGroup className="mt2">
+        {categories.map(category => {
+          return (
+            <ListGroup.Item> 
+              <div className="category-item">
+                <div>
+                  {category} 
+                </div>
+                <div>
+                %
+                </div>
+              </div>
+              
+             
+              
+              
+            </ListGroup.Item>
+          )
+      })}
+        </ListGroup>
+      </div>
+     
+    </div>
   </div>
-);
+  );
+}
+
+
 
 export default App;
--- src/components/StocksList.tsx
+++ src/components/StocksList.tsx
@@ -7,21 +7,73 @@ export const StocksList = ({ stocksList, onRemove }) => {
   if (stocksList.length === 0) {
     return <p className="mt2">No stocks added</p>
   }
+  // return (
+  //   <ListGroup className="mt2">
+  //     {stocksList.map(stockItem => {
+  //       return (
+  //         <ListGroup.Item> <Link to={`/list/${stockItem['id']}`}>{stockItem.tiker} </Link>
+  //           ${stockItem.buyPrice}
+  //           <button 
+  //             type="submit" 
+  //             className="btn btn-outline-primary ml2"
+  //             onClick={() => onRemove(stockItem.id)}
+  //           >
+  //             Remove Stock
+  //           </button>
+  //         </ListGroup.Item>
+  //       )
+  //     })}
+  //   </ListGroup>
+  // )
+
   return (
-    <ListGroup className="mt2">
-      {stocksList.map(stockItem => {
-        return (
-          <ListGroup.Item> <Link to={`/list/${stockItem['id']}`}>{stockItem.tiker} </Link>
-            <button 
-              type="submit" 
-              className="btn btn-outline-primary ml2"
-              onClick={() => onRemove(stockItem.id)}
-            >
-              Remove Stock
-            </button>
-          </ListGroup.Item>
-        )
-      })}
-    </ListGroup>
-  )
+    <div>
+      <table className="table">
+        <thead className="thead-light">
+          <tr>
+            <th>ID</th>
+            <th>Tiker</th>
+            <th>Buy Price</th>
+            <th>Number of Shares</th>
+            <th>Sector</th>
+            <th>Remove</th>
+          </tr>
+        </thead>
+        <tbody>
+          {stocksList.map((item) => {
+            return (
+              <tr>
+                <td>
+                  {item.id}
+                </td>
+                <td>
+                  {item.tiker}
+                </td>
+                <td>
+                  {item.buyPrice}
+                </td>
+                <td>
+                  {item.numShares}
+                </td>
+                <td>
+                  {item.sector}
+                </td>
+                <td>
+                <button 
+                  type="submit" 
+                  className="btn btn-outline-danger ml2"
+                  onClick={() => onRemove(item.id)}
+                >
+                  Remove Stock
+                </button>
+                </td>
+              </tr>
+            )
+          }
+          )}
+
+        </tbody>
+      </table>
+    </div>
+  );
 }
